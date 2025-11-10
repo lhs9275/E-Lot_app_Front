8adabcc553d35a5eb19739677afed3f37408c075
my <hslee9275@gmail.com>
2025-11-10 16:05:42 +0900

    Add OCKey ignore

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'screens/welcom.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart'; // 추가
 
 void main() {
   KakaoSdk.init(
-    nativeAppKey: 'd480d92012b558a0f40dbebbf3a17519', // .env나 dart-define으로 숨겨두면 더 좋음
-    javaScriptAppKey: 'f10081a6b592d61a653e809d9c7acd2c',
+    nativeAppKey: dotenv.env['KAKAO_NATIVE_APP_KEY'] ??'', // .env나 dart-define으로 숨겨두면 더 좋음
+    javaScriptAppKey:dotenv.env['KAKAO_JAVASCRIPT_APP_KEY'] ??'',
   );
   runApp(const MyApp());
 }
